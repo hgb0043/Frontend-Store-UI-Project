8e58c18e3393939fe7e701d1fc716f1e89e3259e
hgb0043 <hayesblackstone@gmail.com>
2025-05-21 09:47:05 -0500

    Display welcome message in home.html if login information is correct

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,12 @@
-const correctFirstName = localStorage.getItem('first-name');
-const correctLastName = localStorage.getItem('last-name');
+/* Signup information created by the user (from sign-up.js) */
 const correctEmailAddress = localStorage.getItem('email-address');
 const correctPassword = localStorage.getItem('password');
 
 const eye = document.querySelector('.js-eye');
 const passwordBox = document.querySelector('.js-password-box');
 const logInButton = document.querySelector('.js-log-in-button');
+const logInLink = document.querySelector('.js-log-in-link')
+const errorMessage = document.querySelector('.js-error-message');
 let email;
 let password;
 
@@ -19,8 +20,19 @@ eye.addEventListener('click', () => {
 });
 
 logInButton.addEventListener('click', () => {
-  email = document.querySelector('.js-email-box').value; 
-  password = document.querySelector('.js-password-box').value;
 
-  console.log(`Form ready to be submitted, ${email}, ${password}`);
+  // Store user attempted login information
+  inputEmailAddress = document.querySelector('.js-email-box').value; 
+  inputPassword = document.querySelector('.js-password-box').value;
+
+  if (inputEmailAddress === correctEmailAddress && inputPassword === correctPassword) {
+    document.querySelector('.js-log-in-link').href = 'home.html';
+    localStorage.setItem('login-status', 'logged-in');
+  } else if (inputEmailAddress === correctEmailAddress && inputPassword !== correctPassword) {
+    errorMessage.innerHTML = 'Password is incorrect. Please try again.';
+    console.log('Password entered is incorrect. Please try again.');
+  } else {
+    errorMessage.innerHTML = 'Email address not recognized. Please try again.';
+    console.log('Email address not recognized. Please try again.')
+  }
 });
