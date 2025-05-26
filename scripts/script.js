e0eb263c14312ec5134b6dd7a929294e52858dff
hgb0043 <hayesblackstone@gmail.com>
2025-05-26 10:17:07 -0500

    Make arrow correctly switch between up and down

--- scripts/script.js
+++ scripts/script.js
@@ -7,6 +7,7 @@ const healthPicksElement = document.getElementById('js-health-products-container
 const loginStatus = localStorage.getItem('login-status') || '';
 const accountMessage = document.querySelector('.account-message');
 const downArrow = document.querySelector('.js-down-arrow');
+const upArrow = document.querySelector('.js-up-arrow');
 const dropHeader = document.querySelector('.js-drop-header');
 const header = document.querySelector('header');
 
@@ -19,13 +20,20 @@ if (loginStatus === 'logged-in') {
 
 // Make drop-header funtional
 downArrow.addEventListener('click', () => {
-  if (dropHeader.classList.contains('hidden')) {
-    header.style.height = '90px';
-    dropHeader.style.height = '40px';
-  } else if (!dropHeader.classList.contains('hidden')) {
-    header.style.height = '50px';
-  }
-  dropHeader.classList.toggle('hidden');
+  header.style.height = '90px';
+  dropHeader.style.height = '40px';
+  dropHeader.classList.remove('hidden');
+  downArrow.classList.add('hidden');
+  console.log(downArrow.classList.contains('hidden'));
+  upArrow.classList.remove('hidden');
+})
+
+upArrow.addEventListener('click', () => {
+  header.style.height = '50px';
+  dropHeader.style.height = '0';
+  dropHeader.classList.add('hidden');
+  downArrow.classList.remove('hidden');
+  upArrow.classList.add('hidden');
 })
 
 /* Makes drop-header retract if window is big enough to display header links in main header */ 
@@ -33,12 +41,14 @@ window.addEventListener('resize', () => {
   if (window.innerWidth > 1200) {
     header.style.height = '50px';
     dropHeader.style.height = '0';
-  } else if (window.innerWidth <= 1200 && !dropHeader.classList.contains('hidden')) {
+
+    downArrow.classList.add('hidden');
+    upArrow.classList.add('hidden');
+  } else if (window.innerWidth <= 1200) {
     header.style.height = '50px';
     dropHeader.style.height = '0';
-    dropHeader.classList.add('hidden')
-    console.log('testing');
-    console.log(dropHeader.classList.contains('hidden'))
+
+    downArrow.classList.remove('hidden');
   }
 })
 
